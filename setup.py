83e8ddbcc093aef94b70eb23e3ab70e8a6212de9
Joshua Boyd <jaboydfamily@gmail.com>
2022-09-05 22:48:55 -0400

    fixed package structure - added gitignore for wheel build files

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='GrabYahooStock',
+				version='0.1',
+				description='Gets stock data from yahoo finance',
+				url='#',
+				author='me',
+				install_requires=['requests', 'bs4', 'pandas'],
+				author_email='',
+				packages=setuptools.find_packages(),
+				zip_safe=False)
